b0e408a2690a7424eb4ae36451a26f3e8de25152
MTianY <mtystar@qq.com>
2018-08-28 22:57:52 +0800

    first

---
+++ block/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  block
+//
+//  Created by 马天野 on 2018/8/28.
+//  Copyright © 2018年 Maty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
